31a6ff9dc99b97ccdffe0c1cae198deb5e5edda1
Michele Scipioni <scipioni.michele@gmail.com>
2019-09-26 18:54:34 +0200

    fix import in DisplayNode/examples

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ export SRC_FOLD=${PWD}/src/
 export NB_FOLD=${PWD}/notebooks/
 
 docker system prune -f
-nvidia-docker build -t occiput:devel -f config/occiput.Dockerfile .
-nvidia-docker run --runtime=nvidia --rm --init -it -v $NB_FOLD:/home/occiput/notebooks -v $SRC_FOLD:/home/occiput/code -p 8888:8888 -p 10000:8080 occiput:devel
+nvidia-docker build -t occiput:test -f config/occiput.Dockerfile .
+nvidia-docker run --runtime=nvidia --rm --init -it -v $NB_FOLD:/home/occiput/notebooks -v $SRC_FOLD:/home/occiput/code -p 8888:8888 -p 10000:8080 occiput:test
